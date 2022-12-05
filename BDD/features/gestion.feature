f79d6fb6beaa288788cb7467971ccd16be4f05c0
geekmoments <cesar@geek-moments.com>
2022-12-05 08:48:51 -0500

    primer test de prueba

---
+++ BDD/features/gestion.feature
@@ -0,0 +1,10 @@
+# language: es 
+# encoding: utf-8
+
+Característica: Gestionar las parejas almacenadas en la lista
+
+Escenario: Agregar un elemento a una lista vacía
+    Dada una lista vacia
+    Cuando se agrega la pareja {'clave': 'valor'}
+    Entonces la lista contiene 1 elemento
+
---
+++ BDD/features/steps_definitions/lista_steps.js
@@ -0,0 +1,16 @@
+const {Given, When, Then} = require('@cucumber/cucumber');
+const expect = require('chai').expect;
+
+Given('una lista vacia',function(){
+
+    return 'pending';
+});
+
+When('se agrega la pareja {}',function(pareja){
+
+    return 'pending';
+});
+Then('la lista tiene {int} elemento almacenado',function(cantidad){
+    
+        return 'pending';
+    });
